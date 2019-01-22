a9433aa38bba399a39e46e919ae9a5b20656f5b7
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2019-01-22 14:15:04 -0600

    Update main.js

--- factoring/main.js
+++ factoring/main.js
@@ -1,3 +1,20 @@
 function factor(){
-
+  var a = document.getElementById("a").value;
+  var b = document.getElementById("b").value;
+  var c = document.getElementById("c").value;
+  var equ = "("+a+"x^2)"+"+"+b+"x"+"+"+c;
+  var factored = "";
+  factored += f(a, b, c);
+  
 }
+
+function f(a, b, c){
+  
+}
+
+2x^2, 4x, 5
+
+
+(2x, 2)(x, 1)
+
+2x^2 + 2x + 2x + 
